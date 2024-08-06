267486994a5927a81056d0a5dc5240b7dadbaa49
Kalina <kalina.nenova@paisii.info>
2024-08-06 15:23:45 +0300

    second commit

---
+++ FahrenheitToCelsius/Converter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FahrenheitToCelsius
+{
+    public class Converter
+    {
+        public static double FahrenheitToCelsius(double F)
+        {
+            double C = 5 / 9 * (F - 32);
+
+            return C;
+        }
+
+    }
+}
